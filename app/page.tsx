f4566c47805cfd8c3d343f0c5841222f62ea77c2
Daniel Sadiq <danielsadiq93@gmail.com>
2025-10-02 22:18:03 +0100

    ui update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
 export default function Home() {
   return (
-    <h1>Hello Next</h1>
+    <div>
+      <h1>My next app</h1>
+      <Link href="/cabins">Explore Cabins</Link>
+    </div>
   );
 }
